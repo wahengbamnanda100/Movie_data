c1b1fc18361ce051cb8d8bcfed4d9f4da67f7901
nandakumar <wahengbamnanda@gmail.com>
2019-09-23 13:18:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/movie_data/movieInput.java
@@ -0,0 +1,31 @@
+package com.example.movie_data;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class movieInput extends Fragment {
+
+
+    public movieInput() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_input, container, false);
+    }
+
+}
